436f16f6dd718bcfd26e3121658b1b6892113ea5
Travis Davis <me@tdavis.dev>
2026-05-07 20:06:10 -0500

    feat(nginx): redirect logs to stdout/stderr

--- templates/basic/flake.nix
+++ templates/basic/flake.nix
@@ -41,6 +41,10 @@
                 {
                   services.nginx = {
                     enable = true;
+                    config = ''
+                      error_log /dev/stderr info;
+                      access_log /dev/stdout;
+                    '';
                     virtualHosts.default = {
                       default = true;
                       locations."/".return = "200 'Hello World'";
